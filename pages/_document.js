002610c407f6dc0ebfaf8dbd74cc3ff7606d03ee
dimple-patel07 <dimple@dimples-MacBook-Pro.local>
2024-03-29 13:16:26 +0530

    Set fevicon icon

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
 		<Html lang="en">
 			<Head>
 				<title>Perk Pickle</title>
+				<link rel="shortcut icon" href="/favicon.ico" />
 			</Head>
 			<body>
 				<Main />
