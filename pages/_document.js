341d0a35333c5da08c7c35d281b940aea6ad49e3
dimple-patel07 <dimple@dimples-MacBook-Pro.local>
2024-04-22 17:12:11 +0530

    added meta title and description

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,8 @@ export default function Document() {
 				{/* <script async src="https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}" /> */}
 				<script async src="https://www.googletagmanager.com/gtag/js?id=G-RQY2Y6S4XJ" />
 
-				<title>Perk Pickle</title>
+				<title>Perk Pickle | https://www.perkpickle.com/ </title>
+				<meta name="description" content="Gain Easy Access To All Your Credit Card Rewards, With PerkPickle, you will no longer miss out on a single reward again" key="desc" />
 				<link rel="shortcut icon" href="/favicon.ico" />
 			</Head>
 			<body>
